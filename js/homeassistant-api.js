c4ed1f38141128eb8b68b1e25405dc92340d1cee
Michaël Arnauts <michael.arnauts@gmail.com>
2016-11-05 23:07:12 +0100

    Fetch bootstrap at startup and add callService function to invoke a Service in HASS.

--- js/homeassistant-api.js
+++ js/homeassistant-api.js
@@ -9,6 +9,9 @@ function HomeAssistantApi(ha_url, update_callback) {
     /** @var array entities The internal representation of the entities */
     this.entities = [];
 
+    /** @var array entities The configuration fetched from HASS */
+    this.configuration = [];
+
     /** @var string ha_url The url to Home Assistant */
     this.ha_url = ha_url;
 
@@ -16,7 +19,56 @@ function HomeAssistantApi(ha_url, update_callback) {
     this.update_callback = update_callback;
 
     /**
-     * Get the data at the specified URL
+     * Calls the specified service in Home Assistant.
+     *
+     * Example:
+     * HomeAssistantApi.callService('light', 'toggle', {'entity_id': 'light.living_room'}, function(result) {
+     *   console.log(result);
+     * });
+     *
+     * @param domain
+     * @param service
+     * @param data
+     * @param callback
+     */
+    this.callService = function (domain, service, data, callback) {
+
+        // Setup new AJAX call
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', this.ha_url + '/api/services/' + domain + '/' + service);
+        xhr.onload = function (e) {
+            if (callback) {
+                var data = JSON.parse(this.response);
+                callback(data);
+            }
+        };
+        xhr.send(JSON.stringify(data));
+    };
+
+    /**
+     * Gets the configuration and initial state.
+     */
+    this.getConfiguration = function () {
+
+        // Setup new AJAX call
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', this.ha_url + '/api/bootstrap');
+        xhr.onload = function (e) {
+            var data = JSON.parse(this.response);
+
+            // Process entities
+            for (var i = 0; i < data.states.length; i++) {
+                HomeAssistantApi.processEntity(data.states[i]);
+            }
+
+            // Store configuration
+            HomeAssistantApi.configuration = data;
+        };
+        xhr.send();
+    };
+
+    /**
+     * Gets the current state of all entities.
      */
     this.getEntities = function () {
 
@@ -33,19 +85,6 @@ function HomeAssistantApi(ha_url, update_callback) {
             }
         };
         xhr.send();
-    };
-
-    /**
-     * Process a single entity and update the UI.
-     * @param entity
-     */
-    this.processEntity = function (entity) {
-
-        // Store entity in memory
-        this.entities[entity.entity_id] = entity;
-
-        // Execute callback
-        this.update_callback(entity);
     };
 
     /**
@@ -67,11 +106,9 @@ function HomeAssistantApi(ha_url, update_callback) {
 
                 // Process entity
                 if (data.event_type == 'state_changed') {
-                    var entity = {};
-                    entity.entity_id = data.data.entity_id;
-                    entity.state = data.data.new_state.state;
-                    HomeAssistantApi.processEntity(entity);
+                    HomeAssistantApi.processEntity(data.data.new_state);
                 }
+
             }, false);
 
             source.addEventListener('open', function (e) {
@@ -88,6 +125,19 @@ function HomeAssistantApi(ha_url, update_callback) {
         } else {
             alert('This browser is not compatible since EventSource is not supported.');
         }
+    };
+
+    /**
+     * Process a single entity and update the UI.
+     * @param entity The entity that received an update.
+     */
+    this.processEntity = function (entity) {
+
+        // Store entity in memory
+        this.entities[entity.entity_id] = entity;
+
+        // Execute callback
+        this.update_callback(entity);
     };
 
 }
